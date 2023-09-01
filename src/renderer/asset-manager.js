489cd36cc4be5e9eb69cb345dcae8c50ebe105e4
olearykieran <koleary515@gmail.com>
2023-09-01 14:17:46 -0400

    Added asset manager, file-explorer, and editor

--- src/renderer/asset-manager.js
+++ src/renderer/asset-manager.js
@@ -1,58 +1,68 @@
-(function() {
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Asset Manager DOMContentLoaded');
     const { ipcRenderer } = require('electron');
-  
+
     // Initialize DOM elements
     const assetList = document.getElementById('asset-list');
-    const addAssetButton = document.getElementById('add-asset');
     
-    // Asset data (you can replace this with your own data structure)
+    // Validate if DOM elements exist
+    if (!assetList) {
+        console.error('Element with ID "asset-list" not found.');
+        return;
+    }
+
+    // Asset data (replace with your data structure)
     const assets = [
-      'asset1.png',
-      'asset2.png',
-      'asset3.png'
+        'asset1.png',
+        'asset2.png',
+        'asset3.png'
     ];
-    
+
     // Load asset list
     function loadAssetList() {
-      assetList.innerHTML = '';
-      assets.forEach((asset) => {
-        const assetElement = document.createElement('div');
-        assetElement.textContent = asset;
-        assetElement.addEventListener('click', () => handleAssetClick(asset));
-        assetList.appendChild(assetElement);
-      });
+        console.log('Loading asset list.');
+        assetList.innerHTML = '';
+        assets.forEach((asset) => {
+            const assetElement = document.createElement('div');
+            assetElement.textContent = asset;
+            assetElement.title = "Click to edit or preview";  // Tooltip
+            assetElement.addEventListener('click', () => handleAssetClick(asset));
+            assetList.appendChild(assetElement);
+        });
     }
-  
+
     // Add new asset
     function addAsset(assetName) {
-      assets.push(assetName);
-      loadAssetList();
+        console.log(`Adding asset: ${assetName}`);
+        assets.push(assetName);
+        loadAssetList();
     }
-    
+
     // Remove asset
     function removeAsset(assetName) {
-      const index = assets.indexOf(assetName);
-      if (index !== -1) {
-        assets.splice(index, 1);
-        loadAssetList();
-      }
+        console.log(`Removing asset: ${assetName}`);
+        const index = assets.indexOf(assetName);
+        if (index !== -1) {
+            assets.splice(index, 1);
+            loadAssetList();
+        }
     }
-    
+
     // Handle asset click
     function handleAssetClick(assetName) {
-      // Implement your logic to open, edit, or preview the asset here
+        console.log(`Asset clicked: ${assetName}`);
+        // Implement your logic to open, edit, or preview the asset here
     }
-    
-    // Load asset list on initialization
+
+    // Initial load
     loadAssetList();
-    
-    // Listen for events to add or remove assets
+
+    // IPC events
     ipcRenderer.on('add-asset', (event, assetName) => {
-      addAsset(assetName);
+        addAsset(assetName);
     });
-    
+
     ipcRenderer.on('remove-asset', (event, assetName) => {
-      removeAsset(assetName);
+        removeAsset(assetName);
     });
-  })();
-  
+});
--- src/renderer/file-explorer.js
+++ src/renderer/file-explorer.js
@@ -1,26 +1,30 @@
-(function() {
-    const { ipcRenderer } = require('electron');
-  
-    const explorerHeader = document.getElementById('explorer-header');
-    const explorerContent = document.getElementById('explorer-content');
-    const toggleExplorer = document.getElementById('toggle-explorer');
-  
-    ipcRenderer.on('update-file-list', (event, files) => {
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('File Explorer DOMContentLoaded');
+  const { ipcRenderer } = require('electron');
+
+  // Initialize DOM elements
+  const explorerContent = document.getElementById('explorer-content');
+
+  // Validate if DOM elements exist
+  if (!explorerContent) {
+      console.error('Essential DOM elements not found.');
+      return;
+  }
+
+  ipcRenderer.on('update-file-list', (event, files) => {
       console.log('Received file list:', files);
-      const fileExplorerDiv = document.getElementById('explorer-content');
-  
-      // Clear previous file list
-      fileExplorerDiv.innerHTML = '';
-  
+      explorerContent.innerHTML = '';
+
       // Add new file list
       files.forEach((file) => {
-        const fileElement = document.createElement('div');
-        fileElement.textContent = file;
-        fileElement.style.color = 'white';
-        fileElement.addEventListener('click', () => {
-          // Handle file click, e.g., open file in editor
-        });
-        fileExplorerDiv.appendChild(fileElement);
+          const fileElement = document.createElement('div');
+          fileElement.textContent = file;
+          fileElement.style.color = 'white';  // You can change this based on file type
+          fileElement.addEventListener('click', () => {
+              console.log(`File clicked: ${file}`);
+              // Handle file click, e.g., open file in editor
+          });
+          explorerContent.appendChild(fileElement);
       });
-    });
-})();
+  });
+});
